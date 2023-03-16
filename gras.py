28ca451d5ab959ef308d4f1b9e43e27e330c90a3
chete-py <112887160+chete-py@users.noreply.github.com>
2023-03-16 10:35:16 +0300

    Update gras.py

--- gras.py
+++ gras.py
@@ -30,7 +30,8 @@ if uploaded_file is not None:
 
         # convert date column to month name
         df = df.iloc[1:]
-        df['Month'] = pd.to_datetime(df['Loss Date', format='%B']).dt.month_name()
+        df['Month'] = pd.to_datetime(df['Loss Date']).dt.month_name()
+        df['Month'] = pd.to_datetime(df['Month'], format='%B')
         df['Day'] = pd.to_datetime(df['Loss Date']).dt.day_name()
         df['Year'] = pd.to_datetime(df['Loss Date']).dt.year
         df.dropna(subset=['Claim No'], inplace=True)
