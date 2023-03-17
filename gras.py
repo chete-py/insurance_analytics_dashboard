5fe76b03c68f4da25fec7bef3a876990224b55e0
chete-py <112887160+chete-py@users.noreply.github.com>
2023-03-17 13:10:31 +0300

    Update gras.py

--- gras.py
+++ gras.py
@@ -141,7 +141,7 @@ if uploaded_file is not None:
 
         
     elif chart_select == "Yearly Claim Analysis":
-        st.plotly_chart(chart_year(df))       
+        st.plotly_chart(fig)       
         
         
     elif chart_select == "View Data Frame":
