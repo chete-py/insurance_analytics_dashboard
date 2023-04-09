0c83a934273424cbd5bb84cae7a8c867df260c3e
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2023-04-09 10:43:13 +0300

    Update gras.py

--- gras.py
+++ gras.py
@@ -89,12 +89,7 @@ def chart_month(df):
     chart.update_yaxes(title='Number of Claims')
     return chart
 
-def chart_year(df):
-    # Group by year and calculate the sum of Claim reserve amount and count of claims
-    agg_df = df.groupby('Year').agg({'Claim reserve amount': 'sum', 'Claim No': 'count'}).reset_index()
-    agg_df = agg_df.rename(columns={'Claim No': 'Number of Claims'})
-
-   
+def chart_year(df):  
 
     # Create the chart with two y-axes
     chart = make_subplots(specs=[[{"secondary_y": True}]])
@@ -145,7 +140,11 @@ if uploaded_file is not None:
 
         
     elif chart_select == "Yearly Claim Analysis":
-        st.plotly_chart(chart_year(df))  
+        st.plotly_chart(chart_year(df))
+            # Group by year and calculate the sum of Claim reserve amount and count of claims
+        agg_df = df.groupby('Year').agg({'Claim reserve amount': 'sum', 'Claim No': 'count'}).reset_index()
+        agg_df = agg_df.rename(columns={'Claim No': 'Number of Claims'})
+
          # Find the year with the highest number of claims
         max_count_year = agg_df.loc[agg_df['Number of Claims'].idxmax(), 'Year']
 
